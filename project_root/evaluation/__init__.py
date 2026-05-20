4cff850f5f9f89506203eea826e57a374befd683
kiwa-liang <1814677538@qq.com>
2026-05-20 12:36:19 +0800

    Delete project_root directory

--- project_root/evaluation/__init__.py
+++
@@ -1,6 +0,0 @@
-from .metrics import compute_metrics
-from .communication_cost import measure_ct_size
-from .dataset_loader import load_dataset
-from .benchmark import benchmark
-
-__all__ = ["compute_metrics", "measure_ct_size", "load_dataset", "benchmark"]
